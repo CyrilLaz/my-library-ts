7b3c98ee5c312480ad454ff1ec745ea37d54dcfd
Cyril_Lazar <klazar@mail.ru>
2024-05-23 22:10:12 +0700

    make the result of methods promises

--- src/interfaces/BooksRepository.ts
+++ src/interfaces/BooksRepository.ts
@@ -1,9 +1,9 @@
 import IBook from "./Book.interface";
 
-export default abstract class BooksRepository {
-  abstract createBook(book: IBook): IBook; // создание книги.
-  abstract getBook(id: string): IBook; // получение книги по id.
-  abstract getBooks(): IBook[]; // получение всех книг.
-  abstract updateBook(id: string): IBook; // обновление книги.
-  abstract deleteBook(id: string): void; // удаление книги.
+export abstract class BooksRepository {
+  abstract createBook(book: IBook): Promise<IBook>; // создание книги.
+  abstract getBook(id: string): Promise<IBook>; // получение книги по id.
+  abstract getBooks(): Promise<IBook[]>; // получение всех книг.
+  abstract updateBook(id: string): Promise<IBook>; // обновление книги.
+  abstract deleteBook(id: string): Promise<void>; // удаление книги.
 }
