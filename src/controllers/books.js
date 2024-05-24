1a88d18ea104ece069794d240293d945761176f7
Cyril_Lazar <klazar@mail.ru>
2024-05-24 22:35:35 +0700

    change controller with container

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,6 +1,8 @@
 /**
  *@typedef {import("../../types.js").TController} TController
  */
+const { myContainer } = require("../container.js");
+const { BooksRepository } = require("../interfaces/BooksRepository.js");
 const { Book } = require("../models/Book.js");
 
 /**@type TController */
@@ -41,15 +43,14 @@ const renderUpdateBookView = (req, res) => {
 /**@type TController */
 const createBook = async (req, res, next) => {
   const { body } = req;
-  console.log(body);
+
   if (!body.title) {
     res.status(400).json({ error: "No Title" });
     return;
   }
-
-  const newBook = new Book(body);
+  const bookRepo = myContainer.get(BooksRepository);
   try {
-    await newBook.save();
+    await bookRepo.createBook(body);
     res.redirect("/books");
   } catch (error) {
     res.status(500).json(error);
@@ -57,32 +58,39 @@ const createBook = async (req, res, next) => {
 };
 
 /**@type TController */
-const downloadBookById = (req, res, next) => {
+const downloadBookById = async (req, res, next) => {
   const { id } = req.params;
+  const bookRepo = myContainer.get(BooksRepository);
 
-  const file = req.db.books.find((book) => book.id === id);
-
-  if (!file) {
-    res.status(404).send({ error: "file not exist" });
-    return;
-  }
-  res.download(file.fileBook, file.fileName, (err) => {
-    if (err) {
-      res.status(500).send({ error: "Error File" });
+  try {
+    const book = await bookRepo.getBook(id);
+    if (!book) {
+      res.status(404).send({ error: "file not exist" });
+      return;
     }
-  });
+
+    res.download(file.fileBook, file.fileName, (err) => {
+      if (err) {
+        res.status(500).send({ error: "Error File" });
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
 };
 /**@type TController */
 const editBook = async (req, res) => {
   const { id } = req.params;
   const { body } = req;
 
+  const bookRepo = myContainer.get(BooksRepository);
+
   if (!body.favorite) {
     body.favorite = false;
   }
 
   try {
-    await Book.findByIdAndUpdate(id, body);
+    await bookRepo.updateBook(id, body);
     res.redirect(`/books/${id}`);
   } catch (error) {
     res.status(500).json(error);
@@ -90,16 +98,16 @@ const editBook = async (req, res) => {
 };
 
 /**@type TController */
-const deleteBook = (req, res) => {
+const deleteBook = async (req, res) => {
   const { id } = req.params;
-  const idx = db.books.findIndex((book) => book.id === id);
 
-  if (!~idx) {
-    res.status(404).send();
-    return;
+  const bookRepo = myContainer.get(BooksRepository);
+  try {
+    await bookRepo.deleteBook(id);
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json(error);
   }
-  db.books.splice(idx, 1);
-  res.send("ok");
 };
 
 module.exports = {
--- src/interfaces/BooksRepository.ts
+++ src/interfaces/BooksRepository.ts
@@ -4,6 +4,6 @@ export abstract class BooksRepository {
   abstract createBook(book: IBook): Promise<IBook>; // создание книги.
   abstract getBook(id: string): Promise<IBook>; // получение книги по id.
   abstract getBooks(): Promise<IBook[]>; // получение всех книг.
-  abstract updateBook(id: string): Promise<IBook>; // обновление книги.
+  abstract updateBook(id: string, obj: Partial<IBook>): Promise<IBook>; // обновление книги.
   abstract deleteBook(id: string): Promise<void>; // удаление книги.
 }
